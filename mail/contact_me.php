d15689cb7eb870339a5dc160273bf32927be5e83
marble <marbleandgranitesolutions2020@gmail.com>
2020-10-26 00:40:27 -0400

    psuh

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,39 @@
-<?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'marcguerra91@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
-?>
+<!--Contact Section-->
+<br>
+  <div class="container">
+    <h2 class="text-center text-uppercase">Contact Me</h2>
+    <div>
+      <form action="https://formspree.io/Marcguerra91@gmail.com" method="POST">
+        <div class="control-group">
+          <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" placeholder="Your Name" id="name" required
+              data-validation-required-message="Please enter your name" name="name">
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+        <div class="control-group">
+          <div class="form-group">
+            <label>Email Address</label>
+            <input type="email" class="form-control" placeholder="Your Email" id="email" required
+              data-validation-required-message="Please enter your email" name="replyto">
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+        <div class="control-group">
+          <div class="form-group">
+            <textarea class="form-control" rows="7" placeholder="Your Message" id="message" required
+              data-validation-required-message="Please leave a message" name="message"></textarea>
+            <p class="help-block text-danger"></p>
+          </div>
+        </div>
+        <div class="text-center">
+          <div id="success"></div>
+          <button type="submit" class="btn btn-lg btn-black">
+            <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+            Send Message
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
